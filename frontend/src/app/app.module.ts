b743e4dac6d1d48627107fe86491d6696d074ee1
DahoGB <dilshodjon.gb@gmail.com>
2019-09-16 20:21:16 +0500

    Add file.component.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileComponent } from './file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileComponent
   ],
   imports: [
     BrowserModule,
